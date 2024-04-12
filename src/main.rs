d4824cd349bc7f8a5aa3bbc9fd51264f638fd82f
tiffgerst <tiffgerst@gmail.com>
2024-04-12 17:19:38 +0100

    convert types and argparsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
+use core::str;
 use starknet::core::types::FieldElement;
 use starknet::core::utils::cairo_short_string_to_felt;
-use num_bigint::BigUint;
-
+use hex::FromHex;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -11,6 +11,14 @@ struct Args {
     /// felt, string or hex to convert
     input: String,
 
+    #[arg(short, long, action)]
+    string: bool,
+    
+    #[arg(short, long, action)]
+    felt: bool,
+    
+    #[arg(long, action)]
+    hex: bool,
 }
 
 #[derive(Debug)]
@@ -20,9 +28,15 @@ enum InputType {
     String,
 }
 
+struct ReturnType {
+    hex: Option<String>,
+    felt: Option<String>,
+    string: Option<String>,
+}
+
 trait ParseType {
     fn find_and_validate_type(&self) -> Result<InputType, String>;
-    fn convert_type(&self, input_type: InputType);
+    fn convert_type(&self, input_type: InputType, felt: bool, hex: bool, string: bool) -> Result<ReturnType, String>;
 }
 
 impl ParseType for String {
@@ -34,14 +48,13 @@ impl ParseType for String {
                 return Err(format!("Invalid hexadecimal string: {}", self));
             }
         } 
-        // else if BigUint::from_radix_be(self.as_bytes(), 190).is_some() {
-        //     if FieldElement::from_dec_str(self).is_err(){
-        //         return Err(format!("Invalid felt (felt too long): {}", self));
-        //     } else {
-        //         return Ok(InputType::Felt);
-        //     }
-            
-        // }
+        else if self.chars().any(|c| c.is_numeric()) {
+            if FieldElement::from_dec_str(self).is_err(){
+                return Err(format!("Invalid felt (felt too long): {}", self));
+            } else {
+                return Ok(InputType::Felt);
+            }
+        }
         else {
             if self.chars().count() <= 31 {
                 return Ok(InputType::String);
@@ -51,19 +64,52 @@ impl ParseType for String {
         }
     }
 
-    fn convert_type(&self, input_type: InputType) {
+    fn convert_type(&self, input_type: InputType, mut felt: bool, mut hex: bool, mut string: bool)-> Result<ReturnType, String> {
+        let mut response: ReturnType = ReturnType {hex: None, felt: None, string: None};
         match input_type {
             InputType::Hex => {
-                let felt = FieldElement::from_hex_be(&self).unwrap();
-                println!("Felt: {:?}", felt.to_string());
+                if !hex && !felt && !string {
+                    felt = true;
+                    hex = true;
+                    string = true;
+                }
+                if hex {response.hex = Some(self.to_string());}
+                if felt {response.felt = Some(FieldElement::from_hex_be(&self).unwrap().to_string());}
+                if string {
+                    let s = String::from(self);
+                    let buffer = <[u8; 12]>::from_hex(&s[2..]).ok();
+                    match buffer {
+                        Some(buffer) => {
+                            let string = str::from_utf8(&buffer).unwrap_or("");
+                            response.string = Some(string.to_string());
+                        }
+                        None => {response.string = None;}      
+                    }
+                }
+                
+                return Ok(response);
             }
             InputType::Felt => {
-                let felt = FieldElement::from_dec_str(self).unwrap();
+                if !hex && !felt && !string {
+                    felt = true;
+                    hex = true;
+                    string = true;
+                }
+                if hex {response.hex = Some("hello".to_string())}
+                if felt {response.felt = Some(FieldElement::from_dec_str(self).unwrap().to_string());}
+                if string {response.string = Some("hello".to_string())}
+                return Ok(response);
             }
             InputType::String => {
-                println!("Felt {}", cairo_short_string_to_felt(self).unwrap().to_string());
-                println!("Hex 0x{}", hex::encode(self));
-               
+                if !hex && !felt && !string {
+                    felt = true;
+                    hex = true;
+                    string = true;
+                }
+                if hex {response.hex = Some(hex::encode(self))}
+                if felt {response.felt = Some(cairo_short_string_to_felt(self).unwrap().to_string());}
+                if string {response.string = Some(self.to_string())}
+                return Ok(response);  
             }
     
     }
@@ -76,10 +122,20 @@ fn main() {
     let args = Args::parse();
 
     let input = args.input;
-    
+    println!("Input: {}", input);
     match input.find_and_validate_type() {
         Ok(input_type) => {
-            input.convert_type(input_type);
+            let result = input.convert_type(input_type, args.felt, args.hex, args.string);
+            match result {
+                Ok(result) => {
+                    println!("Hex: {:?}", result.hex);
+                    println!("Felt: {:?}", result.felt);
+                    println!("String: {:?}", result.string);
+                }
+                Err(error_message) => {
+                    eprintln!("{}", error_message);
+                }
+            }
                 } 
         Err(error_message) => {
             eprintln!("{}", error_message);
