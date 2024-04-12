0fcd815fb39c0cf391ef5a79ee17db772f02c5f1
tiffgerst <tiffgerst@gmail.com>
2024-04-12 12:33:43 +0100

    [main]:validating input type

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,65 @@
 use clap::Parser;
 
+
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    /// felt, string or hex to convert
+    input: String,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+}
+
+enum InputType{
+    Felt,
+    String,
+    Hex
+}
+
+trait ParseType {
+    fn find_type(&self) -> InputType;
+    fn validate_type(&self, input_type: InputType) -> bool;
+}
+
+impl ParseType for String {
+    fn find_type(&self) -> InputType {
+        if self.starts_with("0x") {
+            return InputType::Hex;
+        } else if self.parse::<u64>().is_ok() {
+            return InputType::Felt;
+        } else {
+            return InputType::String;
+        }
+    }
+
+    fn validate_type(&self, input_type: InputType) -> bool {
+        match input_type {
+            InputType::Felt => {
+                println!("Is a felt {}", args.input);
+            }
+            InputType::String => {
+                println!("Is a string {}", args.input);
+            }
+            InputType::Hex => {
+                println!("Is a hex {}", args.input);
+            }
+        }
+    }
+
 }
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    match args.input.find_type() {
+        InputType::Felt => {
+            println!("Is a felt {}", args.input);
+        }
+        InputType::String => {
+            println!("Is a string {}", args.input);
+        }
+        InputType::Hex => {
+            println!("Is a hex {}", args.input);
+        }
     }
 }
